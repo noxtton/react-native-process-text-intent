af44beb9a415596b56a2977563f4e2ec9b95142d
NodariMosia <66739334+NodariMosia@users.noreply.github.com>
2025-05-14 13:49:30 +0400

    Remove non-existing file import in ProcessTextIntent.h

--- ios/ProcessTextIntent.h
+++ ios/ProcessTextIntent.h
@@ -1,12 +1,6 @@
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNProcessTextIntentSpec.h"
-
-@interface ProcessTextIntent : NSObject <NativeProcessTextIntentSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface ProcessTextIntent : NSObject <RCTBridgeModule>
-#endif
 
 @end
